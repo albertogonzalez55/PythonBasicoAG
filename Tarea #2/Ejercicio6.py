2c139ece5a58c7a0159b11e2f298bebe237d1cb9
albertogonzalez55 <122846260+albertogonzalez55@users.noreply.github.com>
2023-01-27 11:06:53 -0600

    Delete Ejercicio6.py

--- Tarea #2/Ejercicio6.py
+++
@@ -1,9 +0,0 @@
-Lista = [1, 2, 2, 3, 3, 4, 4, 5, 6, 8, 8]
-
-resultantList = []
- 
-for element in Lista:
-    if element not in resultantList:
-        resultantList.append(element)
-
-print(resultantList)
